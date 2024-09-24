1e47d28e647891bfb6ef60cd8e459eaa971f087f
nbeeckmans <nicolas.beeckmans@hotmail.com>
2024-09-23 20:34:44 -0400

    finaliser facture

--- src/Bonus/FinaliserFacture/Facture.java
+++ src/Bonus/FinaliserFacture/Facture.java
@@ -1,4 +1,19 @@
 package Bonus.FinaliserFacture;
 
+import java.util.ArrayList;
+
 public class Facture {
+    private ArrayList<Produit> produits = new ArrayList<>();
+
+    public void addProduit(Produit produit) {
+        this.produits.add(produit);
+    }
+
+    public double getPrixTotal() {
+        double prix = 0.0;
+        for (Produit produit : produits) {
+            prix += produit.prix();
+        }
+        return prix;
+    }
 }
